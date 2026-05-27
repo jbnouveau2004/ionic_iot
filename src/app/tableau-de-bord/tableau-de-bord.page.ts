53db4e027f8d8066a635c40268e1dbf3fa128f02
jbnouveau2004 <jb.storti@gmail.com>
2026-05-27 05:14:55 +0200

    fonctionne en local avec /tableaudebord sans entrer quoi que ce soit

--- src/app/tableau-de-bord/tableau-de-bord.page.ts
+++ src/app/tableau-de-bord/tableau-de-bord.page.ts
@@ -3,6 +3,10 @@ import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { IonContent, IonHeader, IonTitle, IonToolbar, IonRange, IonButton } from '@ionic/angular/standalone';
 
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Token } from '@angular/compiler';
+import { firstValueFrom } from 'rxjs';
+
 
 @Component({
   selector: 'app-tableau-de-bord',
@@ -13,15 +17,140 @@ import { IonContent, IonHeader, IonTitle, IonToolbar, IonRange, IonButton } from
 })
 export class TableauDeBordPage implements OnInit {
 
-  constructor() {
+  constructor(private http: HttpClient) {
+
+
    }
 
+titre = 'Tableau de bord';
+voyant1 = '';
+voyant2 = '';
+voltage = '-.-- V';
+tension = 0;
+TOKEN = '';
+
+
+busy = false;
+gpioMessage = '';
+
   ngOnInit() {
+
+
+setInterval(async() => {
+
+  if (this.busy) return;
+
+  this.busy = true;
+
+  try {
+
+    const headers = new HttpHeaders({
+      'Authorization': 'Bearer ' + '2755'
+    });
+
+    const data: any = await firstValueFrom(
+
+      this.http.post(
+        'https://192.168.1.20/status',
+        {},
+        {
+          headers: headers
+        }
+      )
+
+    );
+
+    if (data.gpio == 1) {
+
+      this.gpioMessage = 'La vanne 1 est ouverte';
+      this.voyant1 = 'vert';
+
+    } else {
+
+      this.gpioMessage = 'La vanne 1 est fermée';
+      this.voyant1 = 'rouge';
+
+    }
+
+    this.voltage = data.voltage + ' V';
+
+  } catch (e: any) {
+
+    console.log('Erreur updateValues:', e.message);
+
+  } finally {
+
+    this.busy = false;
+
   }
 
-@Input() titre = 'Tableau de bord';
-@Input() voyant1 = '';
-@Input() voyant2 = '';
-@Input() voltage = '-.-- V';
+}, 5000);
+
+
+
+
+this.vanne2(event);
+
+
+this.changerTension(event);
+
+
+  }
+
+
+async vanne2(event: any) {
+
+  const headers = new HttpHeaders({
+    'Authorization': 'Bearer ' + '2755'
+  });
+
+  this.http.post(
+    'https://192.168.1.20/togglevanne2',
+    {},
+    {
+      headers: headers
+    }
+  ).subscribe({
+
+    next: (reponse) => {
+      console.log('Toggle vanne2 envoyé', reponse);
+    },
+
+    error: (e) => {
+      console.log('Erreur vanne2:', e.message);
+    }
+
+  });
+}
+
+
+async changerTension(event: any) {
+
+  const headers = new HttpHeaders({
+    'Authorization': 'Bearer ' + '2755',
+    'X-PWM': this.tension
+  });
+
+  this.http.post(
+    'https://192.168.1.20/pwm',
+    {},
+    {
+      headers: headers
+    }
+  ).subscribe({
+
+    next: (reponse) => {
+      console.log('PWM envoyé', reponse);
+    },
+
+    error: (e) => {
+      console.log('Erreur PWM:', e.message);
+    }
+
+  });
+
+}
+
+
 
 }
